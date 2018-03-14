ef206a77f2058b80944e94ca7e89ba1005adacfa
Shubham Mathur <shubham.mathur@engineer.com>
2018-03-14 05:31:43 +0530

    Responses updated

--- src/components/Responses/Responses.jsx
+++ src/components/Responses/Responses.jsx
@@ -1,63 +1,68 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import './Responses.css';
 
 const rand = require('random-key');
 
-
-class Responses extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      responses: [],
-      formName: props.data.formName,
-    };
-  }
-
-  getResponses=responses => responses.map((response, index) =>
-    (
-      <p
-        className={index % 2 === 0 ? 'Response-even' : 'Response-odd'}
-        key={rand.generate(5)}
-      >
-        {response}
+const getResponses = responses => responses.map((response, index) =>
+  (
+    <p
+      className={index % 2 === 0 ? 'Response-even' : 'Response-odd'}
+      key={rand.generate(5)}
+    >
+      {response}
+    </p>
+  ));
+const Responses = (props) => {
+  const questions = props.data.data.map(question => (
+    <div className="Question-container" key={rand.generate(5)}>
+      <p className="Question-statement">
+        {question.question}
       </p>
-    ))
-
-
-  render() {
-    const questions = this.props.data.data.map(question => (
-      <div className="Question-container" key={rand.generate(5)}>
-        <p className="Question-statement">
-          {question.question}
-        </p>
-        <p className="Question-heading-message">
+      <p className="Question-heading-message">
       Last 7 responses
-        </p>
-        <div className="Question-responses">
-          {this.getResponses(question.responses)}
-        </div>
+      </p>
+      <div className="Question-responses">
+        {getResponses(question.responses)}
       </div>
-    ));
-    return (
-      <div className="Responses">
-        <div className="Responses-header" />
-        <div className="Responses-body" >
-          <div className="Responses-Main-Box" >
-            <p className="Responses-title">{this.state.formName}</p>
-            <div className="Responses-questions-container">
-              {questions}
-            </div>
-            <button
-              className="Responses-submit-button"
-              onClick={() => this.props.onClick()}
-              type="button"
-            >Go Back
-            </button>
+    </div>
+  ));
+  return (
+    <div className="Responses">
+      <div className="Responses-header" />
+      <div className="Responses-body" >
+        <div className="Responses-Main-Box" >
+          <p className="Responses-title">{props.data.formName}</p>
+          <div className="Responses-questions-container">
+            {questions}
           </div>
+          <button
+            className="Responses-submit-button"
+            onClick={() => props.onClick()}
+            type="button"
+          >Go Back
+          </button>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+
+Responses.propTypes = {
+  data: PropTypes.shape({
+    formName: PropTypes.string,
+    data: PropTypes.arrayOf(PropTypes.shape({
+      question: PropTypes.string,
+      responses: PropTypes.arrayOf(PropTypes.string),
+    })),
+  }),
+  onClick: PropTypes.func,
+};
+
+Responses.defaultProps = {
+  data: null,
+  onClick: () => null,
+};
 
 export default Responses;
